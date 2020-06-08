e65f976acad123af5dbf5dcdb62f3db16fe05f4e
AnnKemp <ann.kemp@scarlet.be>
2020-06-08 16:24:56 +0200

    added some comment in the app.module.ts page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; /* deze moet je dus in de module steken om die [(ngModel)] in de html te laten werken (ik krijg in elk geval geen foutmeldingen meer maar nog wél altijd nen NaN */
+import { FormsModule } from '@angular/forms'; /* deze moet je dus in de module steken om die [(ngModel)] in de html te laten werken */
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
