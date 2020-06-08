d3c4468f86359c90778202258fcfe69124b01d6d
AnnKemp <ann.kemp@scarlet.be>
2020-06-08 16:08:03 +0200

    the calculating works fine! But now I am adding some control to avoid empty fields and got some errors here

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,17 +11,41 @@ export class AppComponent {
   num2: number;
   result: number;
   add(){
+    this.check();
     this.result = this.num1 + this.num2;
   }
   substract(){
+    this.check();
     this.result = this.num1 - this.num2;
   }
   multiply(){
+    this.check();
     this.result = this.num1 * this.num2;
   }
   divide(){
+    this.check();
     this.result = this.num1 % this.num2;
   }
+  check() {
+    // hier nog een foutje op te lossen waarschijnlijk neemt functie isNaN niet alles aan
+    if (((!isNaN(this.num1)) && (!isNaN(this.num2))) {
+      this.wrong();
+    }else{
+      this.right();
+    }
+  }
+  wrong(){
+    /* dat heeft natuurlijk niet als id input maar als tagname input en het moet
+    alleen dat een specifiek veld zijn dat rood wordt en daarna moet het natuurlijk terug wit worden dus*/
+    document.getElementById('input').style.backgroundColor = '#ff0000';
+    /* via this het juiste veld selecteren en het event meegeven
+     en niet onclick maar al tijdens het intypen checken en als ze juist typen terug dat rood weg */
+    }
+    right(){
+    /* en hier van 't zelfde */
+      document.getElementById('input').style.backgroundColor = '#fff';
+    }
 }
+
 
 
