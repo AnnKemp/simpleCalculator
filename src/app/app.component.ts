d38fcb8de93cd99cd4b490c71731678ef81244b8
AnnKemp <ann.kemp@scarlet.be>
2020-06-09 14:33:19 +0200

    the error-controle is working with a check for both fields, not the fields seperately

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ export class AppComponent {
   title = 'Calculator App';
   num1: number;
   num2: number;
-  result: number;
+  result: any;
   add(){
     this.check();
     this.result = this.num1 + this.num2;
@@ -34,11 +34,18 @@ export class AppComponent {
     }
   }
   // hier moet ik nog even aanpassen dat het inputfield met de fout in rood wordt en niet het andere
+  // hier ligt het antwoord in waarom ie die style niet verdraagt omdat het type van x fout is
+  // //omdat dat een element is: https://indepth.dev/exploring-angular-dom-manipulation-techniques-using-viewcontainerref/
+  // ik zou hier gebruik moeten maken van as om de typering van x in any te veranderen
   wrong(){
-    document.querySelector('input').style.backgroundColor = '#ff0000';
+    document.getElementsByTagName('input')[0].style.backgroundColor = '#ff0000';
+    document.getElementsByTagName('input')[1].style.backgroundColor = '#ff0000';
+    document.getElementsByTagName('p')[0].innerHTML = 'Some of the fields are empty, please fill in a number to complete the calculation.';
     }
   right(){
-      document.querySelector('input').style.backgroundColor = '#fff';
+    document.getElementsByTagName('input')[0].style.backgroundColor = '#fff';
+    document.getElementsByTagName('input')[1].style.backgroundColor = '#fff';
+    document.getElementsByTagName('p')[0].innerHTML = '';
     }
 }
 
